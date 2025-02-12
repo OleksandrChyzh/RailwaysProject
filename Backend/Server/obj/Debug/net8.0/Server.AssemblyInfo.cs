529fec5c786475d9b017159ec8ce09953e514718
OleksandrChyzh <147910360+OleksandrChyzh@users.noreply.github.com>
2025-02-12 12:58:36 +0200

    corrected configuration csproj

--- Backend/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Backend/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4528e811e7306ad30d3584fb7083798d6adfce53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be41725f646d937fce074388deabdfe78980d7b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
