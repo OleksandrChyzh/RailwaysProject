a712ae089f3c987ad2ee408bd0e72cd8c17caec6
OleksandrChyzh <147910360+OleksandrChyzh@users.noreply.github.com>
2025-02-13 22:30:35 +0200

    Created .gitingnore and .gitattributes

--- Backend/BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
+++ Backend/BusinessLogic/obj/Debug/net8.0/BusinessLogic.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce1533d2c5f2940461e0d587ea762f0e2652b54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa5243bf88fb2c7d0e4ed8211fd53df59dc4ca6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
