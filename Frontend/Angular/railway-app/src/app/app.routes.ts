0dd007e042bcf52d0e604a0278a40b4e5c1abc59
Artem-Yurchenko-cyber <yurchenko.artem13@gmail.com>
2025-03-05 22:09:49 +0200

    updated files (started do the decomposition)

--- Frontend/Angular/railway-app/src/app/app.routes.ts
+++ Frontend/Angular/railway-app/src/app/app.routes.ts
@@ -17,11 +17,13 @@ import { HomeComponent } from './home/home.component';
 import { MyTicketsComponent } from './my-tickets/my-tickets.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ScheduleComponent } from './schedule/schedule.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Головна сторінка
   { path: 'my-tickets', component: MyTicketsComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'schedule', component: ScheduleComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' } // Перенаправлення, якщо шлях невідомий
 ];
