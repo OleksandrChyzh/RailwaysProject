ad512fad124b27b80737f964271084803790f6db
Artem-Yurchenko-cyber <yurchenko.artem13@gmail.com>
2025-03-19 18:49:42 +0200

    Added registration component
    I added registration button to main page. And added some text to page registration

--- Frontend/Angular/railway-app/src/app/app.routes.ts
+++ Frontend/Angular/railway-app/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { MyTicketsComponent } from './my-tickets/my-tickets.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Головна сторінка
   { path: 'my-tickets', component: MyTicketsComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'schedule', component: ScheduleComponent },
+  { path: 'registration', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' } // Перенаправлення, якщо шлях невідомий
 ];
